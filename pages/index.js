6883fd73a414ef935cae77e6b8695b5a98029619
Yahya Makarim <ymakarim@gmail.com>
2021-09-23 11:12:27 +0500

    frameworks, types and sections pages done

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import ComponentsFilter from '../components/ComponentsFilter'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -6,6 +7,18 @@ export default function Home() {
       <div className='px-4 py-6 sm:px-0'>
         Homepage: <br />
         is reusable - done
+        <br />
+        <Link href='/frameworks/bootstrap'>
+          <a>Go to pages/frameworks/[slug].js</a>
+        </Link>
+        <br />
+        <Link href='/types/free'>
+          <a>Go to pages/types/[slug].js</a>
+        </Link>
+        <br />
+        <Link href='/sections/footer'>
+          <a>Go to pages/sections/[slug].js</a>
+        </Link>
         <hr />
         <div className='bg-white p-8 my-8'>
           <ComponentsFilter addonFilter='&framework.slug=bootstrap' />
--- pages/sections/[slug].js
+++ pages/sections/[slug].js
@@ -1,10 +1,9 @@
 import ComponentsFilter from '../../components/ComponentsFilter'
 import { useRouter } from 'next/router'
 import react, { useState, useEffect } from 'react'
-const Frameworks = () => {
+const Sections = () => {
   const router = useRouter()
   const { slug } = router.query
-
   const [slugnow, setSlugnow] = useState(router.query.slug)
 
   useEffect(() => {
@@ -14,14 +13,14 @@ const Frameworks = () => {
   return (
     <div className='max-w-7xl mx-auto py-6 sm:px-6 lg:px-8'>
       <div className='px-4 py-6 sm:px-0'>
-        Framework Filter: {slugnow}
+        Section Filter: {slugnow}
         {console.log('SLUG NOW: ', slugnow)}
         <hr />
         <div className='bg-white p-8 my-8'>
-          {slugnow != undefined && <ComponentsFilter addonFilter={`&framework.slug=${slugnow}`} />}
+          {slugnow != undefined && <ComponentsFilter addonFilter={`&section.slug=${slugnow}`} />}
         </div>
       </div>
     </div>
   )
 }
-export default Frameworks
+export default Sections
